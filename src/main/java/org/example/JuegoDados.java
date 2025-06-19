f8a1df837c0ded9e78a5e28b892277aa386de4a9
Benjamin <benjaminurrea14@gmail.com>
2025-06-19 13:18:04 -0400

    Desarrollo clase JuegoDados

--- src/main/java/org/example/JuegoDados.java
+++ src/main/java/org/example/JuegoDados.java
@@ -4,4 +4,13 @@ public class JuegoDados {
     private Dado dado1;
     private Dado dado2;
 
-}
+    public JuegoDados() {
+        dado1 = new Dado();
+        dado2 = new Dado ();
+    }
+
+    public void jugar() {
+        dado1.lanzar();
+        dado2.lanzar();
+    }
+
