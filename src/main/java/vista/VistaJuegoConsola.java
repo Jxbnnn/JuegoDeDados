7be28621e7118d50bde4531bc7d8630e4bcd7e08
francisca <f.aguayo03@ufromail.cl>
2025-06-19 09:42:43 -0400

    clase VistaJuegoConsola

---
+++ src/main/java/vista/VistaJuegoConsola.java
@@ -0,0 +1,37 @@
+package vista;
+
+import java.util.Scanner;
+
+public class VistaJuegoConsola {
+    private Scanner scanner;
+
+    public VistaJuegoConsola() {
+        scanner = new Scanner(System.in);
+    }
+
+    public int mostrarMenu() {
+        System.out.println("\nMENU");
+        System.out.println("1. Jugar");
+        System.out.println("2. Salir");
+        System.out.print("Opcion: ");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public void mostrarResultado(int dado1, int dado2, int suma, boolean gano) {
+        System.out.println("Dado 1: " + dado1);
+        System.out.println("Dado 2: " + dado2);
+        System.out.println("Suma: " + suma);
+        System.out.println(gano ? "Ganaste" : "Perdiste");
+    }
+
+    public boolean confirmarSalida() {
+        System.out.print("Salir: S/N ");
+        String respuesta = scanner.nextLine();
+        return respuesta.equalsIgnoreCase("S");
+    }
+
+    public void cerrarScanner() {
+        scanner.close();
+    }
+}
+
