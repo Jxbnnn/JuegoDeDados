d926ff3eedcf80265922c8378f1d1faad6ac23e8
Benjamin <benjaminurrea14@gmail.com>
2025-06-19 14:04:16 -0400

    Desarrollo Controlador versus

--- src/main/java/controlador/ControladorVersus.java
+++ src/main/java/controlador/ControladorVersus.java
@@ -3,3 +3,7 @@ package controlador;
 import modelo.Jugador;
 import modelo.JuegoVersus;
 import vista.VistaVersusConsola;
+
+public class ControladorVersus {
+    private VistaVersusConsola vista;
+    private JuegoVersus juego;
