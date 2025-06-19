30e074e8a9734404ba3eabeffe9c7e92b95d2bad
Benjamin <benjaminurrea14@gmail.com>
2025-06-19 14:02:52 -0400

    Desarrollo ControladorJuego

--- src/main/java/controlador/ControladorJuego.java
+++ src/main/java/controlador/ControladorJuego.java
@@ -11,3 +11,32 @@ public class ControladorJuego {
         vista = new VistaJuegoConsola();
         juego = new JuegoDados();
     }
+
+    public void iniciar() {
+        boolean salir = false;
+
+        while (!salir) {
+            int opcion = vista.mostrarMenu();
+
+            switch (opcion) {
+                case 1:
+                    juego.jugar();
+                    int d1 = juego.getDado1().getCara();
+                    int d2 = juego.getDado2().getCara();
+                    int suma = juego.getResultado();
+                    boolean gano = juego.haGanado();
+                    vista.mostrarResultado(d1, d2, suma, gano);
+                    break;
+                case 2:
+                    if (vista.confirmarSalida()) {
+                        salir = true;
+                        System.out.println("Gracias por jugar.");
+                    }
+                    break;
+                default:
+                    System.out.println("Opción inválida.");
+            }
+        }
+
+        vista.cerrarScanner();
+    }
