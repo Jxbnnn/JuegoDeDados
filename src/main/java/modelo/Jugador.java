d14bd73451873aeebdd8753d701287ecadff981e
francisca <f.aguayo03@ufromail.cl>
2025-06-19 10:08:49 -0400

    clase Jugador

--- src/main/java/modelo/Jugador.java
+++ src/main/java/modelo/Jugador.java
@@ -1,5 +1,27 @@
 package modelo;
 
 public class Jugador {
-    //
+    private String nombre;
+    private int puntaje;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        this.puntaje = 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void sumarPuntos(int puntos) {
+        this.puntaje += puntos;
+    }
+
+    public int getPuntos() {
+        return puntaje;
+    }
+
+    public void reiniciarPuntaje() {
+        this.puntaje = 0;
+    }
 }
