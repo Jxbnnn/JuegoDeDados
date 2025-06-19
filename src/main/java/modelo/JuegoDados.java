f74fb087c6c46fc4ef945b3672f49c7a02d0cb5b
francisca <f.aguayo03@ufromail.cl>
2025-06-19 09:59:44 -0400

    paquete modelo

--- src/main/java/modelo/JuegoDados.java
+++ src/main/java/modelo/JuegoDados.java
@@ -1,5 +1,16 @@
 package modelo;
 
 public class JuegoDados {
-    //
+    private Dado dado1;
+    private Dado dado2;
+
+    public JuegoDados() {
+        dado1 = new Dado();
+        dado2 = new Dado();
+    }
+
+    public void jugar() {
+        dado1.lanzar();
+        dado2.lanzar();
+    }
 }
